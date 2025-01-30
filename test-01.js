561f80d990f04cb77cf209d5a89b4d4137b8c7dc
zulqarnain-Mughal1 <zakimughal104@gmail.com>
2025-01-30 07:10:58 +0000

    Add new command

--- test-01.js
+++ test-01.js
@@ -1,3 +1,12 @@
-console.log("zulqarnain")
-console.log(4+5)
-console.log(5-2)console.log(4+5)
+// console.log("zulqarnain")
+// console.log(4+5)
+// console.log(5-2)
+// console.log(4+5)
+
+let country = "pak"
+let city = "khanqah dogran"
+const name = "zulqarnain"
+country = "austrailia"
+country = "india"
+name = "ali"
+console.log(country);
---
+++ test-03.js
@@ -0,0 +1,54 @@
+// // // let c = "pakistan"
+// // // console.log(c.length)
+
+// // // let country = "italy"
+// // // console.log(country.toUpperCase())
+// // // console.log(country.toLowerCase())
+
+// // let sen = "Zulqarnain is very dull boy"
+// // let add = "e"
+
+// // // console.log(name.concat(" ",class))
+// // // let first = "Hello";
+// // // let second = "World";
+// // console.log(sen.slice(-10 , -2));  // "Hello World"
+
+
+// // ++++++++++++++++--------Number-+++++++++++++++++++++++++++
+
+// const score = 1000000
+// console.log(score.toLocaleString("en-IN", { style: "currency", currency: "PKR" }));
+
+// let price = 1250.5;
+
+// // console.log(price.toLocaleString("en-US", { style: "currency", currency: "USD" }));
+// // // "$1,250.50"
+
+// // console.log(price.toLocaleString("de-DE", { style: "currency", currency: "EUR" }));
+// // // "1.250,50 €"
+
+// // console.log(price.toLocaleString("ja-JP", { style: "currency", currency: "JPY" }));
+// // // "¥1,251"
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(Math.sqrt(25))
+// console.log(Math.abs(-25))
+// console.log(Math.pow(25))
+// console.log(Math.log10(25))
+// console.log(Math.floor(25.5))
+// console.log(Math.ceil(25.895))
+// console.log(Math.round(25.895))
+max = 8
+min = 1
+console.log(Math.floor(Math.random() * (max- min)+1))
