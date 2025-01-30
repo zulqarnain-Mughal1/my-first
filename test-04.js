a8a36ebb552d25df0ecb1a57cb27997bc10eb6f5
zulqarnain-Mughal1 <zakimughal104@gmail.com>
2025-01-30 08:20:07 +0000

    add some new features in date

--- test-04.js
+++ test-04.js
@@ -1,42 +1,54 @@
-// let myDate = new Date()
+// // let myDate = new Date()
 
-// // console.log(myDate.toDateString())
-// // console.log(myDate.toISOString())
-// // console.log(myDate.toJSON())
-// // console.log(myDate.toLocaleDateString())
-// // console.log(myDate.toLocaleString())
-// // console.log(myDate.toString())
-// // console.log(myDate.toLocaleTimeString())
-// // console.log(myDate.toTimeString())
-// // console.log(myDate.toUTCString())
+// // // console.log(myDate.toDateString())
+// // // console.log(myDate.toISOString())
+// // // console.log(myDate.toJSON())
+// // // console.log(myDate.toLocaleDateString())
+// // // console.log(myDate.toLocaleString())
+// // // console.log(myDate.toString())
+// // // console.log(myDate.toLocaleTimeString())
+// // // console.log(myDate.toTimeString())
+// // // console.log(myDate.toUTCString())
 
-// // console.log(myDate.getDate())
-// // console.log(myDate.getDay())
-// // console.log(myDate.getHours())
-// // console.log(myDate.getFullYear())
-// // console.log(myDate.getMinutes())
-// // console.log(myDate.getMonth()+1)
-// // console.log(myDate.getSeconds())
-// // console.log(myDate.getTime())
+// // // console.log(myDate.getDate())
+// // // console.log(myDate.getDay())
+// // // console.log(myDate.getHours())
+// // // console.log(myDate.getFullYear())
+// // // console.log(myDate.getMinutes())
+// // // console.log(myDate.getMonth()+1)
+// // // console.log(myDate.getSeconds())
+// // // console.log(myDate.getTime())
 
-// // let myCreateDate = new Date(2025,0,23,6,4)
-// let testDate = new Date("02-14-2024")
-// // let myCreatedDate = new Date("01-14-2023")
-// // console.log(myCreateDate.toLocaleString())
+// // // let myCreateDate = new Date(2025,0,23,6,4)
+// // let testDate = new Date("02-14-2024")
+// // // let myCreatedDate = new Date("01-14-2023")
+// // // console.log(myCreateDate.toLocaleString())
 
-// let myOnly = Date.now()
-// // console.log(myOnly)
-// // console.log(testDate.getTime())
+// // let myOnly = Date.now()
+// // // console.log(myOnly)
+// // // console.log(testDate.getTime())
 
-// console.log(testDate.getTime() - myOnly)
+// // console.log(testDate.getTime() - myOnly)
 
-// console.log(Math.floor(myOnly/1000))
+// // console.log(Math.floor(myOnly/1000))
 
-let start = new Date("1996-10-04");
-let end = new Date("2025-02-01");
+// let start = new Date("1996-10-04");
+// let end = new Date("2025-02-01");
 
-let diff = end - start;  // Difference in milliseconds
+// let diff = end - start;  // Difference in milliseconds
 
-// console.log(diff / (1000 * 60 * 60 * 24));  // Convert to days (Output: 31)
+// // console.log(diff / (1000 * 60 * 60 * 24));  // Convert to days (Output: 31)
 
-console.log(Math.floor((diff/(1000*60*60*24))/365))
+// console.log(Math.floor((diff/(1000*60*60*24))/365))
+
+
+let zaki = new Date()
+
+console.log(zaki.toLocaleString("default",{
+    // dateStyle:"full",
+    // dateStyle:"short",
+    // dateStyle:"medium",
+    // dateStyle:"long",
+    era:"long"
+
+}))
